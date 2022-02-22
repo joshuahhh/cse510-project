4879990a16860b19dd33620e5596b81268d33dac
Joshua Horowitz <joshuah@alum.mit.edu>
2022-02-21 17:21:59 -0800

    packaging is always a little trickier than you expect

--- package.sh
+++ package.sh
@@ -2,7 +2,8 @@
 
 rm -rf package
 mkdir package
-cp -r use-case-1 use-case-2 index.html package
+cp -r use-case-1 use-case-2 package
+cp index-package.html package/index.html
 sed -i '' 's/showTool: true/showTool: false/g' package/use-case-*/*.html
 zip -r -X package.zip package
 rm -rf package
